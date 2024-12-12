05627e0108b161b0cb2632c3a037200aedb4df74
zaka123 <zakakhan12628@gmail.com>
2024-12-12 17:23:23 +0500

    charts has been added for revenues.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,6 +2,25 @@
 @section('currentPage')
 Dashboard
 @endsection
+@section('css')
+<style>
+  .apexcharts-menu {
+    background-color: #333333 !important;
+    /* Dark background for better contrast */
+    color: #ffffff;
+    /* White text for visibility */
+    min-width: 130px !important;
+  }
+
+  .apexcharts-menu-item {
+    text-align: center
+  }
+
+  .apexcharts-menu-item:hover {
+    color: black;
+  }
+</style>
+@endsection
 @section('content')
 <div class="row">
   <div class="col-xxl-8 d-flex align-items-stretch">
@@ -51,15 +70,15 @@ Dashboard
         <div class="card w-100 rounded-4" style="height: 120px">
           <div class="card-body h-100">
             <a href="{{ route('customers.index') }}">
-            <div class="d-flex align-items-start justify-content-between mb-1" style="height: 30%">
-              <div class="">
-                <h5 class="mb-0">Total Members</h5>
+              <div class="d-flex align-items-start justify-content-between mb-1" style="height: 30%">
+                <div class="">
+                  <h5 class="mb-0">Total Members</h5>
+                </div>
               </div>
-            </div>
-            <div class="d-flex align-items-end justify-content-end" style="height: 70%">
-              <h1 class="display-6 m-0 fw-bold text-success">{{ $totalCustomers->count() }}</h1>
-            </div>
-          </a>
+              <div class="d-flex align-items-end justify-content-end" style="height: 70%">
+                <h1 class="display-6 m-0 fw-bold text-success">{{ $totalCustomers->count() }}</h1>
+              </div>
+            </a>
           </div>
         </div>
       </div>
@@ -83,14 +102,14 @@ Dashboard
         <div class="card w-100 rounded-4" style="height: 120px">
           <div class="card-body h-100">
             <a href="/expired-members">
-            <div class="d-flex align-items-start justify-content-between mb-1" style="height: 30%">
-              <div class="">
-                <h5 class="mb-0">Expired Members</h5>
+              <div class="d-flex align-items-start justify-content-between mb-1" style="height: 30%">
+                <div class="">
+                  <h5 class="mb-0">Expired Members</h5>
+                </div>
               </div>
-            </div>
-            <div class="d-flex align-items-end justify-content-end" style="height: 70%">
-              <h1 class="display-6 m-0 fw-bold text-danger">{{ $expiredCustomers->count() }}</h1>
-            </div>
+              <div class="d-flex align-items-end justify-content-end" style="height: 70%">
+                <h1 class="display-6 m-0 fw-bold text-danger">{{ $expiredCustomers->count() }}</h1>
+              </div>
             </a>
           </div>
         </div>
@@ -99,19 +118,49 @@ Dashboard
         <div class="card w-100 rounded-4" style="height: 120px">
           <div class="card-body h-100">
             <a href="/upcoming-renewal">
-            <div class="d-flex align-items-start justify-content-between mb-1" style="height: 30%">
-              <div class="">
-                <h5 class="mb-0">Upcoming Renewels</h5>
+              <div class="d-flex align-items-start justify-content-between mb-1" style="height: 30%">
+                <div class="">
+                  <h5 class="mb-0">Upcoming Renewels</h5>
+                </div>
               </div>
-            </div>
-            <div class="d-flex align-items-end justify-content-end" style="height: 70%">
-              <h1 class="display-6 m-0 fw-bold text-info">{{ $expiringCustomers->count() }}</h1>
-            </div>
+              <div class="d-flex align-items-end justify-content-end" style="height: 70%">
+                <h1 class="display-6 m-0 fw-bold text-info">{{ $expiringCustomers->count() }}</h1>
+              </div>
             </a>
           </div>
         </div>
       </div>
 
+    </div>
+  </div>
+  <div class="col-xl-12 col-xxl-4 d-flex align-items-stretch">
+    <div class="card w-100 rounded-4">
+      <div class="card-body">
+        <div class="text-center">
+          <h4 class="mb-0">Monthly Revenue</h4>
+        </div>
+        <div class="mt-4" id="monthlyChart"></div>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-12 col-xxl-4 d-flex align-items-stretch">
+    <div class="card w-100 rounded-4">
+      <div class="card-body">
+        <div class="text-center">
+          <h4 class="mb-0">Semiannual Revenue</h4>
+        </div>
+        <div class="mt-4" id="semiAnnualChart"></div>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-12 col-xxl-4 d-flex align-items-stretch">
+    <div class="card w-100 rounded-4">
+      <div class="card-body">
+        <div class="text-center">
+          <h4 class="mb-0">Annual Revenue</h4>
+        </div>
+        <div class="mt-4" id="annualChart"></div>
+      </div>
     </div>
   </div>
 </div>
@@ -128,5 +177,261 @@ Dashboard
 			table.buttons().container()
 				.appendTo( '#recent-renewels_wrapper .col-md-6:eq(0)' );
 		} );
+</script>
+<script>
+  $(function () {
+  "use strict";
+
+  // Chart for Monthly Revenue
+  var options = {
+  series: [{
+    name: "Revenue",
+    data: [4, 10, 12, 17, 25, 30, 40, 55, 68, 55, 14, 90]
+  }],
+  chart: {
+    height: 300,
+    type: 'bar',
+    sparkline: {
+      enabled: false // Set to false to ensure x-axis labels are visible
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: 1,
+    curve: 'smooth',
+    color: ['transparent']
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      gradientToColors: ['#7928ca'],
+      shadeIntensity: 1,
+      type: 'vertical',
+    },
+  },
+  colors: ["#ff0080"],
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      borderRadius: 4,
+      columnWidth: '45%'
+    }
+  },
+  tooltip: {
+    theme: "dark",
+    x: {
+      show: true // Ensure tooltip displays the x-axis category (month)
+    },
+    y: {
+      formatter: function (value) {
+        return "$" + value; // Prefix the value with a dollar sign
+      }
+    }
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    labels: {
+      style: {
+        colors: '#ffffff', // Adjust this for better visibility
+        fontSize: '12px'
+      }
+    }
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: '#ffffff', // Adjust this for better visibility
+        fontSize: '12px'
+      }
+    },
+    title: {
+      text: 'Revenue ($)', // Optional: Add a title to the y-axis
+      style: {
+        color: '#ffffff',
+        fontSize: '14px'
+      }
+    }
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#monthlyChart"), options);
+chart.render();
+
+
+
+
+// chart for semi annual
+
+var options = {
+  series: [{
+    name: "Revenue",
+    data: [4, 10,]
+  }],
+  chart: {
+    height: 300,
+    type: 'bar',
+    sparkline: {
+      enabled: false // Set to false to ensure x-axis labels are visible
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: 1,
+    curve: 'smooth',
+    color: ['transparent']
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      gradientToColors: ['#7928ca'],
+      shadeIntensity: 1,
+      type: 'vertical',
+    },
+  },
+  colors: ["#ff0080"],
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      borderRadius: 4,
+      columnWidth: '45%'
+    }
+  },
+  tooltip: {
+    theme: "dark",
+    x: {
+      show: true // Ensure tooltip displays the x-axis category (month)
+    },
+    y: {
+      formatter: function (value) {
+        return "$" + value; // Prefix the value with a dollar sign
+      }
+    }
+  },
+  xaxis: {
+    categories: ['Jan-Jun', 'Jul-Dec',],
+    labels: {
+      style: {
+        colors: '#ffffff', // Adjust this for better visibility
+        fontSize: '12px'
+      }
+    }
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: '#ffffff', // Adjust this for better visibility
+        fontSize: '12px'
+      }
+    },
+    title: {
+      text: 'Revenue ($)', // Optional: Add a title to the y-axis
+      style: {
+        color: '#ffffff',
+        fontSize: '14px'
+      }
+    }
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#semiAnnualChart"), options);
+chart.render();
+
+
+// chart for yearly
+var options = {
+  series: [{
+    name: "Revenue",
+    data: [4, 10,]
+  }],
+  chart: {
+    height: 300,
+    type: 'bar',
+    sparkline: {
+      enabled: false // Set to false to ensure x-axis labels are visible
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    width: 1,
+    curve: 'smooth',
+    color: ['transparent']
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      gradientToColors: ['#7928ca'],
+      shadeIntensity: 1,
+      type: 'vertical',
+    },
+  },
+  colors: ["#ff0080"],
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      borderRadius: 4,
+      columnWidth: '45%'
+    }
+  },
+  tooltip: {
+    theme: "dark",
+    x: {
+      show: true // Ensure tooltip displays the x-axis category (month)
+    },
+    y: {
+      formatter: function (value) {
+        return "$" + value; // Prefix the value with a dollar sign
+      }
+    }
+  },
+  xaxis: {
+    categories: ['2024', '2025',],
+    labels: {
+      style: {
+        colors: '#ffffff', // Adjust this for better visibility
+        fontSize: '12px'
+      }
+    }
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: '#ffffff', // Adjust this for better visibility
+        fontSize: '12px'
+      }
+    },
+    title: {
+      text: 'Revenue ($)', // Optional: Add a title to the y-axis
+      style: {
+        color: '#ffffff',
+        fontSize: '14px'
+      }
+    }
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#annualChart"), options);
+chart.render();
+
+
+
+});
 </script>
 @endpush
